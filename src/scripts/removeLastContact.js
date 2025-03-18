2dec3d27e715fde8fd57719e70884f8bd1477ffe
SSiukh <siukhsasha@gmail.com>
2025-03-18 13:07:21 +0200

    complete

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,21 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (!contacts.length) {
+      console.log('There are no contacts');
+      return;
+    }
+
+    contacts.pop();
+
+    await writeContacts(contacts);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 removeLastContact();
